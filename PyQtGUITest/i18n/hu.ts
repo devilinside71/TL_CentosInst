bf97af5e9a63537eb617e29d0f1b5aecc70c3441
weblab <laszlo.devil@gmail.com>
2019-01-15 14:30:01 +0100

    tl_lupdate test

--- PyQtGUITest/i18n/hu.ts
+++ PyQtGUITest/i18n/hu.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="hu_HU" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
 <context>
     <name>MainWindow</name>
     <message>
@@ -10,47 +11,7 @@
     <message>
         <location filename="../ui/Ui_MainWindow.py" line="50"/>
         <source>PushButton</source>
-        <translation>Gomb</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="51"/>
-        <source>File</source>
-        <translation>Fájl</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="52"/>
-        <source>Tools</source>
-        <translation>Eszközök</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="53"/>
-        <source>Help</source>
-        <translation>Súgó</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="54"/>
-        <source>Quit</source>
-        <translation>Kilépés</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="55"/>
-        <source>Settings...</source>
-        <translation>BEállítások...</translation>
-    </message>
-    <message>
-        <location filename="../ui/Ui_MainWindow.py" line="56"/>
-        <source>About...</source>
-        <translation>A programról...</translation>
-    </message>
-    <message>
-        <location filename="../ui/mainwindow.py" line="63"/>
-        <source>Confirmation</source>
-        <translation>Megerősítés</translation>
-    </message>
-    <message>
-        <location filename="../ui/mainwindow.py" line="64"/>
-        <source>Are you sure you want to exit the program?</source>
-        <translation>Biztosan ki akarsz lépni a programból?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../PyQt_quick_Test.py" line="80"/>
@@ -65,6 +26,46 @@
     <message>
         <location filename="../PyQt_quick_Test.py" line="82"/>
         <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="51"/>
+        <source>File</source>
+        <translation>Fájl</translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="52"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="53"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="54"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="55"/>
+        <source>Settings...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Ui_MainWindow.py" line="56"/>
+        <source>About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.py" line="63"/>
+        <source>Confirmation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.py" line="64"/>
+        <source>Are you sure you want to exit the program?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
